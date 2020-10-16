d83e04813c82c9893baa5dcbb09516ac8fd9603e
Siful Islam <sifulsmm@gmail.com>
2020-10-17 00:05:21 +0600

    files added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fileUpload = require('express-fileupload');
 require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eobd6.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eobd6.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
 
 const app = express();
